bb3325488bef114cf5cd7b8fcefeb8215cbbcf28
bobby <bobkibiwott@gmail.com>
2022-03-28 15:20:53 +0300

    UI/UX for the following pages completed Learn page => Ongoing added => Navigation drawer
    pending
    1.Resources
    
    Signed-off-by: bobby <bobkibiwott@gmail.com>

--- app/src/main/java/com/example/idlp/MainActivity.java
+++ app/src/main/java/com/example/idlp/MainActivity.java
@@ -2,37 +2,70 @@ package com.example.idlp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.example.idlp.fragments.Forum;
 import com.example.idlp.fragments.Learn;
 import com.example.idlp.fragments.Profile;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    private ActionBar toolbar;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    DrawerLayout drawerLayout;
+//    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        navigation.setOnItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        toolbar=getSupportActionBar();
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         toolbar.setTitle("Home");
         loadFragment(new Learn());
+
+        ////////
+        navigationView = findViewById(R.id.nav_view);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+
+
+
+        //toggle button
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this , drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
-    private NavigationBarView.OnItemSelectedListener mOnNavigationItemSelectedListener
-            = new NavigationBarView.OnItemSelectedListener() {
+    private NavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new NavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             Fragment fragment;
@@ -41,16 +74,19 @@ public class MainActivity extends AppCompatActivity {
                     toolbar.setTitle("Home");
                     fragment = new Learn();
                     loadFragment(fragment);
+                    drawerLayout.closeDrawer(GravityCompat.START);
                     return true;
                 case R.id.nav_forum:
                     toolbar.setTitle("Forum");
                     fragment = new Forum();
                     loadFragment(fragment);
+                    drawerLayout.closeDrawer(GravityCompat.START);
                     return true;
                 case R.id.nav_profile:
                     toolbar.setTitle("Profile");
                     fragment = new Profile();
                     loadFragment(fragment);
+                    drawerLayout.closeDrawer(GravityCompat.START);
                     return true;
             }
 
